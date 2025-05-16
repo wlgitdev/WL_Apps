bb437e4cb61176ef5e6cbec94c16f69fbf3080eb
WL <wengellatham_dev@outlook.com>
2025-05-16 10:35:26 +0100

    Batch File Changes

--- copyFilesFromProject.bat
+++ copyFilesFromProject.bat
@@ -1,43 +1,58 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Get source and destination paths from user
+:: Get user input
 set /p SOURCE_PATH="Enter source path: "
 set /p DEST_PATH="Enter destination path: "
 set /p PROJECT_NAME="Enter project sub folder name: "
 
-:: Create destination directories if they don't exist
-mkdir "%DEST_PATH%\apps\web\src\api\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\web\src\pages\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\web\src\components\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\web\src\registry\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\web\src\schemas\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\server\src\config\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\server\src\controllers\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\server\src\models\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\server\src\routes\%PROJECT_NAME%" 2>nul
-mkdir "%DEST_PATH%\apps\server\src\services\%PROJECT_NAME%" 2>nul
+:: Define directory structures in arrays
+set "WEB_DIRS=api registry schemas pages components"
+set "SERVER_DIRS=config controllers models routes services"
+
+:: Create all destination directories
+for %%d in (%WEB_DIRS%) do mkdir "%DEST_PATH%\apps\web\src\%%d\%PROJECT_NAME%" 2>nul
+for %%d in (%SERVER_DIRS%) do mkdir "%DEST_PATH%\apps\server\src\%%d\%PROJECT_NAME%" 2>nul
 mkdir "%DEST_PATH%\packages\types\src\%PROJECT_NAME%" 2>nul
 
-:: Copy files for directories that don't need flattening
-xcopy "%SOURCE_PATH%\apps\web\src\api\*.*" "%DEST_PATH%\apps\web\src\api\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\apps\web\src\registry\*.*" "%DEST_PATH%\apps\web\src\registry\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\apps\web\src\schemas\*.*" "%DEST_PATH%\apps\web\src\schemas\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\apps\server\src\config\*.*" "%DEST_PATH%\apps\server\src\config\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\apps\server\src\controllers\*.*" "%DEST_PATH%\apps\server\src\controllers\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\apps\server\src\models\*.*" "%DEST_PATH%\apps\server\src\models\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\apps\server\src\routes\*.*" "%DEST_PATH%\apps\server\src\routes\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\apps\server\src\services\*.*" "%DEST_PATH%\apps\server\src\services\%PROJECT_NAME%\" /s /i /y
-xcopy "%SOURCE_PATH%\packages\types\src\*.*" "%DEST_PATH%\apps\packages\types\src\%PROJECT_NAME%\" /s /i /y
-
-:: Flatten pages directory
-for /r "%SOURCE_PATH%\apps\web\src\pages" %%f in (*.*) do (
-    copy "%%f" "%DEST_PATH%\apps\web\src\pages\%PROJECT_NAME%\" /y
+:: Copy web files with conditional placement
+for %%d in (%WEB_DIRS%) do (
+    if exist "%SOURCE_PATH%\apps\web\src\%%d\" (
+        for /f "delims=" %%f in ('dir /b /s "%SOURCE_PATH%\apps\web\src\%%d\*.*"') do (
+            set "fname=%%~nxf"
+            if exist "%DEST_PATH%\apps\web\src\%%d\!fname!" (
+                copy "%%f" "%DEST_PATH%\apps\web\src\%%d\" /y
+            ) else (
+                copy "%%f" "%DEST_PATH%\apps\web\src\%%d\%PROJECT_NAME%\" /y
+            )
+        )
+    )
 )
 
-:: Flatten components directory
-for /r "%SOURCE_PATH%\apps\web\src\components" %%f in (*.*) do (
-    copy "%%f" "%DEST_PATH%\apps\web\src\components\%PROJECT_NAME%\" /y
+:: Copy server files with conditional placement
+for %%d in (%SERVER_DIRS%) do (
+    if exist "%SOURCE_PATH%\apps\server\src\%%d\" (
+        for /f "delims=" %%f in ('dir /b /s "%SOURCE_PATH%\apps\server\src\%%d\*.*"') do (
+            set "fname=%%~nxf"
+            if exist "%DEST_PATH%\apps\server\src\%%d\!fname!" (
+                copy "%%f" "%DEST_PATH%\apps\server\src\%%d\" /y
+            ) else (
+                copy "%%f" "%DEST_PATH%\apps\server\src\%%d\%PROJECT_NAME%\" /y
+            )
+        )
+    )
+)
+
+:: Handle packages/types directory
+if exist "%SOURCE_PATH%\packages\types\src\" (
+    for /f "delims=" %%f in ('dir /b /s "%SOURCE_PATH%\packages\types\src\*.*"') do (
+        set "fname=%%~nxf"
+        if exist "%DEST_PATH%\packages\types\src\!fname!" (
+            copy "%%f" "%DEST_PATH%\packages\types\src\" /y
+        ) else (
+            copy "%%f" "%DEST_PATH%\packages\types\src\%PROJECT_NAME%\" /y
+        )
+    )
 )
 
 echo Copy completed successfully!
